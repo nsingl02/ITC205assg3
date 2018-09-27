78993f593199e4ada6b082fd89508df97b660d96
mendefrost <Mende_.12@hotmail.com>
2018-09-27 15:25:50 +1000

    Added runtime execution tests

--- src/hotel/entities/Hotel.java
+++ src/hotel/entities/Hotel.java
@@ -102,7 +102,6 @@ public class Hotel {
 
 	}
 
-
 	public void checkin(long confirmationNumber) {
 
 		Booking booking = findBookingByConfirmationNumber(confirmationNumber);
@@ -110,17 +109,13 @@ public class Hotel {
 		if(booking == null){
 
 			throw new RuntimeException();
-
 		}
-
 		else {
 
 			booking.checkIn();
 			activeBookingsByRoomId.put(booking.getRoom().getId(),booking);
 		}
 	}
-
-
 
 	public void addServiceCharge(int roomId, ServiceType serviceType, double cost) {
 
